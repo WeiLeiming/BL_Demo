6a9528d3433bbacf076798319cb554f7653f1822
leiming <2008weileiming@gmail.com>
2018-03-05 18:57:46 +0800

    Add

---
+++ playground/JSON.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let data = try? JSONSerialization.data(withJSONObject: ["147460": 0, "147461": 1], options: JSONSerialization.WritingOptions.prettyPrinted)
